30e2151508c53751efe79a5de444779400c24d3b
Yevhenii Pozniak <jekamanu@gmail.com>
2024-04-14 12:45:42 +0300

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,35 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +38,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
